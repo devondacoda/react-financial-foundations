6b6826f14d523beed5245962146f394121e60028
Devon JohnBaptiste <devondevon@outlook.com>
2018-01-18 00:55:27 -0500

    use

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 
 if (module === require.main) {
-  const PORT = 8080;
+  const PORT = process.env.PORT || 8080;
   app.listen(PORT, () => console.log('server listening on port', PORT));
 }
 
